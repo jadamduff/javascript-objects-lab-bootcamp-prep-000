6dc6feb7dce1d76ddbdb01c350d71afa5c43344c
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 05:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {banana_pudding: ['1 banana', 'pudding']}
 
 var updateObjectWithKeyAndValue = function (object, key, value) {
-  console.log(Object.assign({}, object, {key: value}))
+  var newObj = Object.assign({}, object, {key: value})
+  return newObj
 }
